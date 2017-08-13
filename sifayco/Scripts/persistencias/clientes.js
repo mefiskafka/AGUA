f4e5e1765ac060e8e632045a5275ef26af489a5d
vagrant <gambitos19@hotmail.com>
2017-08-13 01:20:38 -0500

    Packages 6

---
+++ sifayco/Scripts/persistencias/clientes.js
@@ -0,0 +1,157 @@
+﻿var listadoClientes = new Array();
+
+//Recuperar todos los clientes
+function recuperarClientes() {
+    $.getJSON("../api/clientes/",
+        function (data) {
+            $.each(data, function (key, val) {
+                var str = '(' + val.Id + ')' + val.Nombre;
+                $('<li/>', { html: str }).appendTo($('#clientes'));
+
+                //Agrego el cliente a la lista de clientes
+                var cliente = {Id: val.Id,Nombre: val.Nombre};
+                listadoClientes.push(cliente);
+
+            });
+        });
+}
+
+//Buscar cliente
+function buscarCliente(id, callback) {
+    $.ajax({
+        url: "../api/clientes/",
+        data: { id: id },
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        statusCode: {
+            200: function (cliente) {
+                callback(cliente);
+            },
+            404: function() {
+                alert("Cliente no encontrado");
+            }
+        }
+    });
+}
+
+$('#buscarCliente').live({
+    click: function () {
+        var id = $('#IdCliente').val();
+        buscarCliente(id, function (cliente) {
+            var str = '(' + cliente.Id + ')' + cliente.Nombre;
+            $('#cliente').html(str);
+        });
+    }
+});
+
+//Crear cliente
+function crearCliente(nuevoCliente, callback) {
+    $.ajax({
+        url: "../api/clientes/",
+        data: JSON.stringify(nuevoCliente),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        statusCode: {
+            201: function(cliente) {
+                callback(cliente);
+            }
+        }
+    });
+}
+
+$('#crearCliente').live({    
+   click:function() {
+       var nuevoCliente = { Nombre: $('#NombreCliente').val() };
+       crearCliente(nuevoCliente, function(cliente) {
+           alert("El nuevo cliente fue creado con el Id " + cliente.Id);
+           
+           var str = '(' + cliente.Id + ')' + cliente.Nombre;
+           $('<li/>', { html: str }).appendTo($('#clientes'));
+           
+           //Agrego el cliente a la lista de clientes
+           listadoClientes.push(cliente);
+       });
+   } 
+});
+
+//Actualizar cliente
+function actualizarCliente(clienteActualizado, callback) {
+    $.ajax({
+        url: "../api/clientes/",
+        data: JSON.stringify(clienteActualizado),
+        type: "PUT",
+        contentType: "application/json;charset=utf-8",
+        statusCode: {
+            200: function (cliente) {
+                callback(cliente);
+            },
+            404: function () {
+                alert("Cliente no encontrado.");
+            }
+        }
+    });
+}
+
+$('#actualizarCliente').live({
+    click: function() {
+        var cliente = {
+            Id: $('#Id').val(),
+            Nombre: $('#Nombre').val()
+        };
+        actualizarCliente(cliente, function () {
+            alert("El cliente fue actualizado.");
+
+            $('#clientes').empty();
+            for (var i = 0; i < listadoClientes.length; i++) {
+                var clienteAux = listadoClientes[i];
+                if (clienteAux.Id == cliente.Id)
+                    clienteAux.Nombre = cliente.Nombre;
+                
+                var str = '(' + clienteAux.Id + ')' + clienteAux.Nombre;
+                $('<li/>', { html: str }).appendTo($('#clientes'));
+            }
+            
+        });
+    }
+});
+
+//Elimnar cliente
+function eliminarCliente(id, callback) {
+    $.ajax({
+        url: "../api/clientes/" + id,
+        type: "DELETE",
+        contentType: "application/json;charset=utf-8",
+        statusCode: {
+            200: function (cliente) {
+                callback(cliente);
+            },
+            404: function () {
+                alert("Cliente no encontrado.");
+            }
+        }
+    });
+}
+
+$('#eliminarCliente').live({
+    click: function () {
+        var id = $('#IdClienteEliminar').val();
+        eliminarCliente(id, function (cliente) {
+            alert("El cliente fue eliminado.");
+            $('#clientes').empty();
+            var listadoClientesNvo = new Array();
+            for (var i = 0; i < listadoClientes.length; i++) {
+                var clienteAux = listadoClientes[i];
+                if (clienteAux.Id != id) {
+                    listadoClientesNvo.push(clienteAux);
+                    var str = '(' + clienteAux.Id + ')' + clienteAux.Nombre;
+                    $('<li/>', { html: str }).appendTo($('#clientes'));
+                }
+            }
+            listadoClientes = listadoClientesNvo;
+        });
+    }
+});
+
+$(document).ready(function () {
+    recuperarClientes();
+});
---
+++ sifayco/Scripts/persistencias/contratos.js
@@ -0,0 +1,64 @@
+﻿//Buscar cliente
+function buscarContrato(id, callback) {
+    $.ajax({
+        url: "../api/Contratos/",
+        data: { id: id },
+        type: "Get",
+        contentType: "application/json;charset=utf-8",
+        statusCode: {
+            200: function (contrato) {
+                callback(contrato);
+            },
+            404: function () {
+                alert("Contrato no encontrado");
+            },
+            500: function() {
+                alert("Error interno de servidor");
+            }
+        },
+        error: function (e) {
+            console.log(e.statusText);
+        },
+        success: function () {
+            console.log("success");
+        },
+    });
+}
+
+function limpiarFormulario() {
+    $('#txtNombre').val("");
+    $('#txtCalle').val("");
+    $('#txtNumInterior').val("");
+    $('#txtNumExterior').val("");
+    $('#txtColonia').val("");
+    $('#txtCorreo').val("");
+    $('#txtTelefono').val("");
+
+    $('#txtSector').val("");
+    $('#txtManzana').val("");
+    $('#txtLote').val("");
+    $('#txtRuta').val("");
+    $('#txtProgresivo').val("");
+    $('#txtHabitantes').val("");
+}
+
+function btnBuscar_onclick() {
+    var id = $('#txtBuscar').val();
+            limpiarFormulario();
+            buscarContrato(id, function (contrato) {
+                $('#txtNombre').val(contrato.T_USUARIO_SERVICIO.NOMBRE_USUARIO);
+                $('#txtCalle').val(contrato.T_USUARIO_SERVICIO.CALLE);
+                $('#txtNumInterior').val(contrato.T_USUARIO_SERVICIO.NUMERO_INTERIOR);
+                $('#txtNumExterior').val(contrato.T_USUARIO_SERVICIO.NUMERO_EXTERIOR);
+                $('#txtColonia').val(contrato.T_USUARIO_SERVICIO.COLONIA);
+                $('#txtCorreo').val(contrato.T_USUARIO_SERVICIO.CORREO_ELECTRONICO);
+                $('#txtTelefono').val(contrato.T_USUARIO_SERVICIO.TELEFONO);
+
+                $('#txtSector').val(contrato.SECTOR);
+                $('#txtManzana').val(contrato.MANZANA);
+                $('#txtLote').val(contrato.LOTE);
+                $('#txtRuta').val(contrato.RUTA);
+                $('#txtProgresivo').val(contrato.PROGRESIVO);
+                $('#txtHabitantes').val(contrato.NUMERO_HABITANTES);
+            });
+}
